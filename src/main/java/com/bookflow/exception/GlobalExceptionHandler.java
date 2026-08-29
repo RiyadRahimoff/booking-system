3b4e90cad737ff6ea14e5d85f71aadd165982313
RiyadRahimoff <riyadrahimov777@gmail.com>
2026-08-30 00:28:25 +0400

    Added GlobalHandler

--- src/main/java/com/bookflow/exception/GlobalExceptionHandler.java
+++ src/main/java/com/bookflow/exception/GlobalExceptionHandler.java
@@ -1,4 +1,18 @@
 package com.bookflow.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<?> handleUserNotFound(UserNotFoundException exception) {
+
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(),exception.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(errorResponse);
+    }
 }
