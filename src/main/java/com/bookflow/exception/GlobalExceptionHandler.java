ad41666c7c906ee9dc6769a2493f5e86b37b1bb9
RiyadRahimoff <riyadrahimov777@gmail.com>
2026-08-31 01:56:31 +0400

    Added new Exception and SecurityConfig

--- src/main/java/com/bookflow/exception/GlobalExceptionHandler.java
+++ src/main/java/com/bookflow/exception/GlobalExceptionHandler.java
@@ -10,9 +10,36 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<?> handleUserNotFound(UserNotFoundException exception) {
 
-        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(),exception.getMessage());
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(), exception.getMessage());
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(errorResponse);
+    }
+
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    public ResponseEntity<?> handleEmailExists(EmailAlreadyExistsException exception) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.CONFLICT.value(), exception.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(errorResponse);
+
+    }
+
+    @ExceptionHandler(InvalidRoleException.class)
+    public ResponseEntity<?> handleRoleException(InvalidRoleException exception) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), exception.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(errorResponse);
+
+    }
+
+    @ExceptionHandler(UserStatusPendingException.class)
+    public ResponseEntity<?> handleUserStatusException(UserStatusPendingException exception) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), exception.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(errorResponse);
+
     }
 }
